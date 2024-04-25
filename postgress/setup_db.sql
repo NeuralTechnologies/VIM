5e6dc9eb84d4389d4c58f8e494c5546bb7b9a089
NeuralTradingio <neuraltrading.git@gmail.com>
2024-04-25 23:06:48 +0400

    commit 1

--- postgress/setup_db.sql
+++ postgress/setup_db.sql
@@ -9,3 +9,10 @@ CREATE DATABASE VIM;
 
 -- Grant all privileges on the new database to the new user
 GRANT ALL PRIVILEGES ON DATABASE VIM TO vim;
+
+CREATE TABLE languages (
+    id SERIAL PRIMARY KEY,
+    language_code VARCHAR(10) UNIQUE NOT NULL,
+    language_name VARCHAR(255) NOT NULL
+);
+
